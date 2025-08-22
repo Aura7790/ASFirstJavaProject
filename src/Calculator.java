aa8688218c50bdc2b7523026536ddcd98def1c9f
Aura Scripcariu <aura.bejenaru@yahoo.com>
2025-08-22 21:05:36 +0300

    - rezolvare Tema Lab 11 - ex1 Polimorfism

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,25 +1,50 @@
 public class Calculator {
-    public int calculateSumOfTwo(int first, int second) {
-        int result;
-        result = first + second;
-        return result;
+    public int calculateSum(int first, int second) {
+        return first + second;
     }
 
-    public float calculateSubstractionOfTwo(float first, float second) {
-        float result;
-        result = first - second;
-        return result;
+    public double calculateSum(double first, double second, double third) {
+        return first + second + third;
+
     }
 
-    public float calculateMultiplicationOfTwo(int first, int second) {
-        float result;
-        result = first * second;
-        return result;
+    public float calculateSum(float first, float second) {
+        return first + second;
     }
 
-    public float calculateDivisionOfTwo(int first, int second) {
-        float result;
-        result = (float) first / second;
-        return result;
+    public int calculateSubstraction(int first, int second, int third, int fourth) {
+        return first - second - third - fourth;
+    }
+
+    public float calculateSubstraction(float first, float second) {
+        return first - second;
+    }
+
+    public double calculateSubstraction(double first, double second) {
+        return first - second;
+    }
+
+    public int calculateMultiplication(int first, int second) {
+        return first * second;
+    }
+
+    public float calculateMultiplication(float first, float second, float third, float fourth) {
+        return first * second * third * fourth;
+    }
+
+    public double calculateMultiplication(double first, double second) {
+        return first * second;
+    }
+
+    public float calculateDivision(float first, float second) {
+       return first / second;
+    }
+
+    public float calculateDivision(int first, int second, int third) {
+        return  (float) first / second / third;
+    }
+
+    public double calculateDivision(double first, double second) {
+        return first / second;
     }
 }
--- src/MainCalculator.java
+++ src/MainCalculator.java
@@ -4,17 +4,25 @@ public class MainCalculator {
         // 1. Sum
         int x = 7;
         int y = -31;
-        System.out.println("The sum of two int numbers is: " + calc.calculateSumOfTwo(x,y));
+        System.out.println("The sum of two int numbers is: " + calc.calculateSum(x,y));
+        System.out.println("The sum of three double numbers is: " + calc.calculateSum(2.2, 3.4566, 6.55));
+        System.out.println("The sum of two float numbers is: " + calc.calculateSum(2.2f, 6.55f));
 
         // 2. Substraction
         float a = 17.521F;
         float b = 5.5F;
-        System.out.println("The difference between two float numbers is: " + calc.calculateSubstractionOfTwo(a,b));
+        System.out.println("The difference between 4 int numbers is: " + calc.calculateSubstraction(7, 9, 66, 8 ));
+        System.out.println("The difference between two float numbers is: " + calc.calculateSubstraction(a,b));
+        System.out.println("The difference between two double numbers is: " + calc.calculateSubstraction(-4.56322, 7.899));
 
         // 3. Multiplication
-        System.out.println("The multiplication of two int numbers is: " + calc.calculateMultiplicationOfTwo(x,y));
+        System.out.println("The multiplication of two int numbers is: " + calc.calculateMultiplication(x,y));
+        System.out.println("The multiplication of 4 float numbers is: " + calc.calculateMultiplication(1.1f, 4.55f, 7.12f, 3.14f));
+        System.out.println("The multiplication of two double numbers is: " + calc.calculateMultiplication(7.77777, 98.552453));
 
         // 4. Division
-        System.out.println("The division of two int numbers is: " + calc.calculateDivisionOfTwo(x,y));
+        System.out.println("The division of 2 float numbers is: " + calc.calculateDivision(7.1f, 5.5555f));
+        System.out.println("The division of 3 int numbers is: " + calc.calculateDivision(7, -3, 9));
+        System.out.println("The division of two double numbers is: " + calc.calculateDivision(7.77777779, 54.3333));
     }
 }
